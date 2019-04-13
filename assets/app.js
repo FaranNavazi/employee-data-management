7ed655880602ec862abce7ead965cc836b286837
Faran Navazi <faran.navazi@gmail.com>
2019-04-13 13:54:48 -0500

    appending data works

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,57 @@
 var config = {
-    apiKey: "AIzaSyD1v5SLU62aJ4JAI_PbNL--IDuwimsFw_k",
-    authDomain: "employee-data-management-21fec.firebaseapp.com",
-    databaseURL: "https://employee-data-management-21fec.firebaseio.com",
-    projectId: "employee-data-management-21fec",
-    storageBucket: "employee-data-management-21fec.appspot.com",
-    messagingSenderId: "376900891882"
-  };
+  apiKey: "AIzaSyD1v5SLU62aJ4JAI_PbNL--IDuwimsFw_k",
+  authDomain: "employee-data-management-21fec.firebaseapp.com",
+  databaseURL: "https://employee-data-management-21fec.firebaseio.com",
+  projectId: "employee-data-management-21fec",
+  storageBucket: "employee-data-management-21fec.appspot.com",
+  messagingSenderId: "376900891882"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var name = "";
-  var role = "";
-  var start = "";
-  var rate = "";
+var name = "";
+var role = "";
+var start = "";
+var rate = "";
 
+$(".btn").on("click", function(event) {
+  event.preventDefault();
+  name = $("#employee-name").val();
+  role = $("#role").val();
+  start = $("#start-date").val();
+  rate = $("#monthly-rate").val();
 
-  $(".btn").on("click", function(event) {
-    event.preventDefault();
-    name = $("#employee-name").val();
-    role = $("#role").val();
-    start = $("#start-date").val();
-    rate = $("#monthly-rate").val();
-
-    database.ref().push({
-      name: name,
-      role: role,
-      start: start,
-      rate: rate
-    })
+  database.ref().push({
+    name: name,
+    role: role,
+    start: start,
+    rate: rate
   });
-  
 
 
-//  function makeRow() {
-//      return `
-//     <tbody>
-//     <tr>
-//       <td>${employeeName}</td>
-//       <td>${role}</td>
-//       <td>${startDate}</td>
-//       <td>${monthsWorked}</td>
-//       <td>${rate}</td>
-//       <td>${totalBilled}</td>
-//     </tr>
-//   </tbody>
-//     `
-//  }
+});
 
+
+
+database.ref().on("child_added", function(snapshot, prevChildKey) {
+  var newTr = snapshot.val();
+  console.log(snapshot.val());
+  $("table").append(makeRow(newTr));
+});
+
+ function makeRow(data) {
+     return `
+    <tbody>
+    <tr>
+      <td>${data.name}</td>
+      <td>${data.role}</td>
+      <td>${data.startDate}</td>
+      <td>${data.monthsWorked}</td>
+      <td>${data.rate}</td>
+      <td>${data.totalBilled}</td>
+    </tr>
+  </tbody>
+    `
+ }
