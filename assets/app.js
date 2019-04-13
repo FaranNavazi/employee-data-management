199a4930b7e0c0e04855906869b7774774b7853f
Spinning Plates <alec.munguia@gmail.com>
2019-04-13 11:32:32 -0500

    form js code

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+var config = {
+    apiKey: "AIzaSyD1v5SLU62aJ4JAI_PbNL--IDuwimsFw_k",
+    authDomain: "employee-data-management-21fec.firebaseapp.com",
+    databaseURL: "https://employee-data-management-21fec.firebaseio.com",
+    projectId: "employee-data-management-21fec",
+    storageBucket: "employee-data-management-21fec.appspot.com",
+    messagingSenderId: "376900891882"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var employee = ["Alec", "Jameson", "Jason", "Faran"];
+  var role = ["CEO", "CEO", "CEO", "CEO"];
+  var start = [];
+  var rate = [100, 100, 100, 100];
+
+
+  document.on("click", button, function() {
+    event.preventDefault();
+    var formEmployee = $("#employee-name").val().trim();
+    var formRole = $("#role").val().trim();
+    var formStart = $("#start-date").val().trim();
+    var formRate = $("#monthly-rate").val().trim();
+    employee.push(formEmployee);
+    role.push(formRole);
+    start.push(formStart);
+    rate.push(formRate);
+  });
+  
+  })
